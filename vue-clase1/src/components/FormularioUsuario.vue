c861c94e9e192b6fa8ca8e3b7ddec27851febfb0
landersoft <landersoft@gmail.com>
2025-10-19 20:49:49 -0300

    se agrega nuevo componente

---
+++ vue-clase1/src/components/FormularioUsuario.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="form">
+        <input v-model="nombre"
+        type="text"
+        placeholder="nombre">
+
+        <input v-model.number="edad"
+        type="number"
+        placeholder="edad">
+
+        <select v-model="selected">
+            <option disabled value="">Seleccione un país</option>
+            <option value="Chile">Chile</option>
+            <option value="Argentina">Argentina</option>
+            <option value="Perú">Perú</option>
+        </select>
+
+        <button @click="enviar">Enviar</button>
+
+        <div v-if="show">
+            <p>Nombre: {{nombre}}</p>
+            <p>Edad: {{edad}}</p>
+            <p>País: {{selected}}</p>
+        </div>
+
+        
+
+    </div>
+    
+
+</template>
+<script setup>
+    import {ref} from 'vue'
+    const nombre = ref('')
+    const edad = ref(0)
+    const selected = ref('')
+    const show = ref(false)
+
+
+
+    function enviar() {
+        show.value = true
+        console.log(nombre.value, edad.value, selected.value)
+    }
+
+</script>
+<style scoped>
+.form {
+    text-align: center;
+    font-family: Arial, sans-serif;
+    margin-top: 10px;
+    
+    
+  }
+</style>
