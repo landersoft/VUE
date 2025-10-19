4fa1355a913558533dfeb64c793698a260d2941d
landersoft <landersoft@gmail.com>
2025-10-18 21:16:38 -0300

    comienzo

---
+++ vue-clase1/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <Greeting nombre ="Rodrigo1"  />
+</template>
+
+<script setup>
+  import {ref} from 'vue'
+  import Greeting from './components/Greeting.vue';
+
+  //const nombre = ref('Rodrigo')
+  //const fecha = new Date().toLocaleDateString()
+</script>
+
+<style>
+  .app {
+    text-align: center;
+    font-family: Arial, sans-serif;
+    margin-top: 100px;
+  }
+</style>
+
+
---
+++ vue-clase1/src/components/Greeting.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="greeting">
+    <h1>Hola {{ nombre }}</h1>
+    <p>La fecha desde este mismo componente es: {{ fecha }}</p>
+    <p>Contador: {{ contador }}</p>
+    <button @click="incrementar">+ Sumar {{ contador}}</button>
+  </div>
+</template>
+
+<script setup>
+  import {ref} from 'vue'
+
+  const props = defineProps({
+    nombre: String,
+    })
+
+  //variables de este mismo componente
+  const fecha = new Date().toLocaleDateString()
+  const contador = ref(0)
+
+  function incrementar() {
+    contador.value++
+    console.log(contador.value)
+  }
+</script>
+
+<style scoped>
+h1{
+    color:black
+}
+  .greeting {
+    text-align: center;
+    font-family: Arial, sans-serif;
+    margin-top: 10px;
+    color:aqua
+    
+  }
+</style>
